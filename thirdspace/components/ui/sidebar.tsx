e750f3e024fdbdc031c48b58e95ffed16273c0c9
Nicholas Rios <rios.nicholas@protonmail.com>
2024-12-30 23:52:18 -0500

    Increase sidebar trigger margins and padding

--- thirdspace/components/ui/sidebar.tsx
+++ thirdspace/components/ui/sidebar.tsx
@@ -271,7 +271,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-8 w-8", className, "m-2")}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
